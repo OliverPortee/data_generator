08dca490aaa2b27188937454b1938106d18896d3
Oliver Portee <oliver.portee@gmail.com>
2022-05-13 18:51:04 +0200

    start to implement cli parser but it's not so much fun

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,60 +1,57 @@
 
-#include <cxxopts.hpp>
-#include <iostream>
-
 #include "data_generation.hpp"
 
+#include <cxxopts.hpp>
+#include <iostream>
+#include <optional>
+#include <limits>
+#include <cstring>
+#include <string>
+#include <sstream>
+
+
+struct CliOptions {
+    int min = std::numeric_limits<int>::min(); // --min
+    int max = std::numeric_limits<int>::max(); // --max
+    unsigned int sample_count = 1000;          // -n
+    unsigned int col_count = 5;                // -c
+    std::optional<unsigned int> seed;          // --seed
+    std::string output = "csv";                // -o
+    std::string distribution = "uniform";      // --distribution
+};
+
+
+template<class T>
+T parse(const char* s) {
+    std::istringstream iss(std::string{s});
+    T t;
+    iss >> t;
+    return t;
+}
+
 Settings parse_cli_parameters(int argc, char* argv[]) {
-    // --min NUMBER
-    // --max NUMBER
-    // -n COUNT # default 1000
-    // -c COUNT # default 5
-    // --seed NUMBER
-    // -o csv|sql # default csv
-    // --distribution uniform|normal # default uniform
-
-    cxxopts::Options options("data generator", "generate random integer data");
-    options.add_options()("min", "minimum of random values",
-                          cxxopts::value<int>())(
-        "max", "maximum of random values",
-        cxxopts::value<
-            int>())("n,samplecount", "number of rows to be generated",
-                    cxxopts::value<
-                        unsigned int>())("a,along", "bla",
-                                         cxxopts::value<
-                                             bool>())("b,blong", "bla",
-                                                      cxxopts::value<bool>());
-    auto result = options.parse(argc, argv);
-
-    // TODO: --help
-    // TODO: catch parse errors
-
-    bool has_min = result.count("min");
-    bool has_max = result.count("max");
-    if (has_min && !has_max) {
-        std::cerr << "must specify --max when using --min" << std::endl;
-        std::exit(0);
-    }
-    if (has_max && !has_min) {
-        std::cerr << "must specify --min when using --max" << std::endl;
-        std::exit(0);
-    }
-    if (has_min && has_max) {
-        if (result["min"].as<int>() >= result["max"].as<int>()) {
-            std::cerr << "minimum value must be smaller than maximum value"
-                      << std::endl;
+    
+    CliOptions options{};
+    unsigned int i = 1;
+    while (i < argc) {
+        if (std::strcmp(argv[i], "--min") == 0) {
+            if (++i >= argc) {
+                // fail
+            }
+            options.min = parse<int>(argv[i]);
+        } else if (std::strcmp(argv[i], "--max") == 0) {
+            if (++i >= argc) {
+                // fail
+            }
+            options.max = parse<int>(argv[i]);
+        } else if (std::strcmp(argv[i], "-n") == 0) {
+            if (++i >= argc) {
+                // fail
+            }
+            options.sample_count = parse<unsigned int>(argv[i]);
+        } else if (std::strcmp(argv[i], "-c") == 0) {
         }
     }
-
-    std::cout << result.count("a") << std::endl;
-    std::cout << result.count("b") << std::endl;
-
-    return Settings{0,
-                    1000000,
-                    5,
-                    10,
-                    std::make_unique<CsvOutput>(),
-                    std::make_unique<UniformDistribution>(0, 1000000)};
 }
 
 int main(int argc, char* argv[]) {
